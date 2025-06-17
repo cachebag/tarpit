f7a3862bf8b4d3e8bd93ae1dceb11941d4b44aa1
Akrm Al-Hakimi <alhakimiakrmj@gmail.com>
2025-06-17 16:33:43 -0400

    tests: added more tests up to chksum

--- tests/header_parser.rs
+++ tests/header_parser.rs
@@ -32,3 +32,65 @@ fn parses_file_mode() -> Result<(), HeaderParseError> {
     Ok(())
 }
 
+#[test]
+fn parses_file_uid() -> Result<(), HeaderParseError> {
+    let mut blk = blank_block();
+    blk[108..116].copy_from_slice(b"0001741\0");
+    let hdr = HeaderUstar::from_bytes(&finalise_block(blk))?;
+    assert_eq!(hdr.file_uid()?, 0o1741);
+    Ok(())
+}
+
+#[test]
+fn parses_file_gid() -> Result<(), HeaderParseError> {
+    let mut blk = blank_block();
+    blk[116..124].copy_from_slice(b"0005710\0");
+    let hdr = HeaderUstar::from_bytes(&finalise_block(blk))?;
+    assert_eq!(hdr.file_gid()?, 0o5710);
+    Ok(())
+}
+
+#[test]
+fn parses_file_size() -> Result<(), HeaderParseError> {
+    let mut blk = blank_block();
+    blk[124..136].copy_from_slice(b"10000000000\0"
+);
+    let hdr = HeaderUstar::from_bytes(&finalise_block(blk))?;
+    assert_eq!(hdr.file_size()?, 0o10000000000);
+    Ok(())
+}
+
+#[test]
+fn parses_file_mtime() -> Result<(), HeaderParseError> {
+    let mut blk = blank_block();
+    blk[136..148].copy_from_slice(b"07347410350\0"); 
+    let hdr = HeaderUstar::from_bytes(&finalise_block(blk))?;
+    assert_eq!(hdr.file_mtime()?, 1000214760);        
+    Ok(())
+}
+
+
+#[test]
+fn parses_file_chksum() -> Result<(), HeaderParseError> {
+    let mut blk = blank_block();
+    blk[0..9].copy_from_slice(b"file.txt\0");
+    blk[257..263].copy_from_slice(b"ustar\0");
+    blk[263..265].copy_from_slice(b"00");
+
+    for b in &mut blk[148..156] {
+        *b = b' ';
+    }
+
+    let sum: u64 = blk.iter().map(|&b| b as u64).sum();
+
+    let chksum_field = format!("{:06o}\0 ", sum);
+    blk[148..156].copy_from_slice(chksum_field.as_bytes());
+
+    let hdr = HeaderUstar::from_bytes(&blk)?;
+
+    assert_eq!(hdr.file_chksum()?, sum);
+
+    Ok(())
+}
+
+
